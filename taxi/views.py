67dd83cbc0a6fda4f78922f011b786aab9aafe4c
Arkadiusz Bałakier <arkadiusz.balakier@gmail.com>
2026-01-12 09:11:25 +0100

    creating validators.py

--- taxi/views.py
+++ taxi/views.py
@@ -93,3 +93,8 @@ class DriverCreateView(LoginRequiredMixin, generic.CreateView):
     model = Driver
     fields = "__all__"
     success_url = reverse_lazy("taxi:driver-list")
+
+
+class DriverDeleteView(LoginRequiredMixin, generic.DeleteView):
+    model = Driver
+    success_url = reverse_lazy("taxi:driver-list")
